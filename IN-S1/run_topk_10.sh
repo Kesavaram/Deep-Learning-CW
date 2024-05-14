717f0f9562ab5852d8151703969bc28c9b2e1939
mirakain01 <lyubomira01@gmail.com>
2024-05-14 16:37:48 +0100

    IN-S1 added

---
+++ IN-S1/run_topk_10.sh
@@ -0,0 +1,25 @@
+
+#!/bin/bash
+
+python ../../train_imgnet_10.py --data_folder_path $1 \
+--resume \
+--name resnet18_topk_10 \
+--topk 2_10 \
+--total_epochs 50 \
+--batch_size 8 \
+--use_sin_val_folder $2 \
+
+
+
+echo "Press 'y' to continue..."
+
+while true; do
+    read -n 1 -s -r key
+    if [[ $key == "y" ]]; then
+        break
+    fi
+done
+
+echo "You pressed: $key"
+
+
