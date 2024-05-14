b8d78bac3186d9d28ca7462bf1a477466aeaed80
Kesavaram <kesavaram994@gmail.com>
2024-05-14 16:09:27 +0100

    Training cnn topk

---
+++ IN-S1/run_topk.sh
@@ -0,0 +1,25 @@
+
+#!/bin/bash
+
+python ../../train_imgnet_10.py --data_folder_path $1 \
+--resume \
+--name resnet18_topk \
+--topk 2_10 \
+--total_epochs 5 \
+--batch_size 8 \
+--use_sin_val_folder $2 \
+
+
+
+echo "Press 'y' to continue..."
+
+while true; do
+    read -n 1 -s -r key
+    if [[ $key == "y" ]]; then
+        break
+    fi
+done
+
+echo "You pressed: $key"
+
+
